76b1ad5d80a89a6e074d7297b4bb8ff71fc88110
Arthur <kingart808@gmail.com>
2019-07-16 10:19:51 -1000

    working on my remove function

--- linkedList.js
+++ linkedList.js
@@ -3,6 +3,65 @@
  * @description  Main Module
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
-function linkedListGenerator(){
 
+function linkedListGenerator() {
+  let head = null;
+  let tail = null;
+  function getHead() {
+    return head;
+  }
+  function getTail() {
+    return tail;
+  }
+  function add(value) {
+    let newNode = {
+      value: value,
+      next: null
+    };
+    if (head === null) {
+      head = newNode;
+    }
+    if (tail !== null) {
+      tail.next = newNode;
+    }
+    tail = newNode;
+    return newNode;
+  }
+  function get(number) {
+     let current = head
+      if(number === 0){
+          return head
+      } 
+      for (let i = 0; i < number; i++) {
+          if(current.next === null){
+              return false;
+          }
+          current = current.next
+      }
+      return current
+  }
+  function remove(number) {
+      let currentNode = head
+      let prevNode;
+      if(number === 0){
+        return currentNode.next
+      }
+      for (let i = 0; i < number; i++) {
+        if(currentNode.next === null){
+          return false;
+        }       
+        prevNode = currentNode
+        currentNode = currentNode.next
+      }
+      prevNode.next = currentNode.next
+  }
+  function insert(value, number) {}
+  return {
+    getHead: getHead,
+    getTail: getTail,
+    add: add,
+    remove: remove,
+    get: get,
+    insert: insert
+  };
 }
